27ea29870b64a54af5dee3daac61e5c574cddf62
Bekmamat <akmataliev95@gmail.com>
2019-08-29 18:16:34 +0300

    Create isPermutationPalindrome.go

---
+++ isPermutationPalindrome.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	str := []rune{'c', 'a', 'r', 'r', 'a', 'c', 'e'}
+	fmt.Println(palindromable(str))
+}
+
+func palindromable(str []rune) bool {
+	oddCountLetters := map[rune]int{}
+	for _, i := range str {
+		if _, ok := oddCountLetters[i]; ok {
+			delete(oddCountLetters, i)
+		} else {
+			oddCountLetters[i] = 1
+		}
+	}
+	// if oddCountLetters length should be equal to 1 or 0
+	return len(oddCountLetters) <= 1
+}
