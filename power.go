4eb3862d0ae8543b24ac5cea4a2228e23d27973a
Bekmamat <akmataliev95@gmail.com>
2020-05-31 12:43:14 +0300

    Create power.go

---
+++ power.go
@@ -0,0 +1,29 @@
+package pow
+
+func Pow(x float64, n int) float64 {
+	if n == 0 || x == 1 || (x == -1 && n % 2 == 0) {
+		return 1
+	}
+	if x == -1 {
+		return x
+	}
+	if n < 0 {
+		x = 1 / x
+        n *= -1
+	}
+	var product float64 = x
+	var result float64 = 1
+	cp := 1
+	for cp != n {
+		cp += cp
+		if cp > n {
+			n = n - (cp / 2)
+			cp = 1
+			result *= product
+			product = x
+			continue
+		}
+		product *= product
+	}
+	return result * product
+}
